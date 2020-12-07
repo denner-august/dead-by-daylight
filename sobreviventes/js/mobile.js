13dec385f6f36b19a16fe4f0826d21cae55af96c
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2020-12-06 22:52:46 -0300

    modificacao da versão mobile

---
+++ sobreviventes/js/mobile.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('.menu')
+const btnmenu = document.querySelector('.btnMenu')
+const btnclose = document.querySelector('.btnMenu_close')
+
+
+btnmenu.addEventListener('click', function(){
+    menu.classList.add('menu_open');
+})
+
+btnclose.addEventListener('click', function(){
+    menu.classList.remove('menu_open');
+})
